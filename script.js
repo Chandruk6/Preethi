8b2079c7c1d856c374f2b48fbb6daa64863e7151
T Sivaranjani <sivaranjanit1062001@gmail.com>
2023-03-24 20:39:28 +0530

    Files upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function hover() {
+    document.getElementById("method-drop").style.display = "block";
+  }
+  function hover1() {
+    document.getElementById("method-drop").style.display = "none";
+  }
+  function stovehover() {
+    document.getElementById("stove-drop").style.display = "block";
+  }
+  function stovehover1() {
+    document.getElementById("stove-drop").style.display = "none";
+  }
+  function Chimneyhover() {
+    document.getElementById("chimeny-drop").style.display = "block";
+  }
+  function chimenyhover1() {
+    document.getElementById("chimeny-drop").style.display = "none";
+  }
+  function otherhover() {
+    document.getElementById("other-drop").style.display = "block";
+  }
+  function otherhover1() {
+    document.getElementById("other-drop").style.display = "none";
+  }
+  function big(img){
+    document.querySelector('.big').src= img;
+}
